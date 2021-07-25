f74e9f7518838ad17813e71037271edd36ff2a00
KevinShiCN <szsyshijiawen@gmail.com>
2021-07-25 17:37:56 +0800

    +增加了一个弹窗用于确认输入的QQ号是否符合预期 +修复了Windows可能会出现的路径问题导致程序无法使用

--- main.py
+++ main.py
@@ -13,36 +13,47 @@ print(qqinput)
 qqlist = qqinput.split()
 print(qqlist)
 
-# 提示框确定开始
-confirm_message = pyautogui.alert(
-    text='请确保已打开QQ小程序权限管理页面', title='程序开始确认框', button='确定！')
-print(confirm_message)
+# 提示框提醒确认QQ号
+confirm_qqlist = pyautogui.confirm(
+    text='请检查是否为预期QQ号码{}'.format(qqlist), title='请检查QQ号', buttons=['确定！', '取消'])
+print(confirm_qqlist)
 
-# 第一次激活输入框
-pyautogui.click(screenWidth / 2, screenHeight / 5.2)
-pyautogui.press('browserrefresh')
-time.sleep(1.5)
+if confirm_qqlist in "确定！":
+    # 提示框确定开始
+    confirm_message = pyautogui.alert(
+        text='请确保已打开QQ小程序权限管理页面', title='程序开始确认框', button='确定！')
+    print(confirm_message)
 
-# 循环填写QQ号
-for qqnum in list(qqlist):
+    # 第一次激活输入框
     pyautogui.click(screenWidth / 2, screenHeight / 5.2)
-    pyautogui.press('tab')
-    pyautogui.write(qqnum, interval=0.01)
-    pyautogui.press('tab')
-    pyautogui.press('enter')
-    time.sleep(0.6)
-    pyautogui.press('tab')
-    pyautogui.press('enter')
-    time.sleep(0.2)
+    pyautogui.press('browserrefresh')
+    time.sleep(1.5)
 
-# 勾选体验者权限并跳转至最后
-tiyan_clickbox = pyautogui.locateCenterOnScreen(
-    r'./screenshots/tiyan_clickbox.png', grayscale=True)
-if tiyan_clickbox is None:
-    pyautogui.alert(
-        text='无法找到体验权限按钮！请手动进行后续操作！', title='错误：无法定位体验权限按钮', button='确定！')
+    # 循环填写QQ号
+    for qqnum in list(qqlist):
+        pyautogui.click(screenWidth / 2, screenHeight / 5.2)
+        pyautogui.press('tab')
+        pyautogui.write(qqnum, interval=0.01)
+        pyautogui.press('tab')
+        pyautogui.press('enter')
+        time.sleep(0.6)
+        pyautogui.press('tab')
+        pyautogui.press('enter')
+        time.sleep(0.2)
+
+    # 勾选体验者权限并跳转至最后
+    tiyan_clickbox = pyautogui.locateCenterOnScreen(
+        'tiyan_clickbox.png', grayscale=True)
+    if tiyan_clickbox is None:
+        pyautogui.alert(
+            text='无法找到体验权限按钮！请手动进行后续操作！', title='错误：无法定位体验权限按钮', button='确定！')
+    else:
+        pyautogui.click(tiyan_clickbox)
+        pyautogui.press('end')
+        pyautogui.alert(
+            text='已完成！请点击【确认添加】', title='成功结束！', button='辛苦了！接下来我自己来！')
+# 用户手动取消
 else:
-    pyautogui.click(tiyan_clickbox)
-    pyautogui.press('end')
-    pyautogui.alert(
-        text='已完成！请点击【确认添加】', title='成功结束！', button='辛苦了！接下来我自己来！')
+    canceled = pyautogui.alert(
+        text='取消执行！', title='执行已取消', button='确定')
+    print(canceled)
